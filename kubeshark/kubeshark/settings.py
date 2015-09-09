3b82387fbb885bb38845154632534a38372c65ff
Alex Couper <amcouper@gmail.com>
2015-09-09 22:05:45 +0000

    Upto date for 0.2

--- kubeshark/kubeshark/settings.py
+++ kubeshark/kubeshark/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -75,14 +75,12 @@ WSGI_APPLICATION = 'kubeshark.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+# To override, simply provide a DATABASE_URL env var.
+# eg DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///{0}/db.sqlite3'.format(BASE_DIR)),
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
